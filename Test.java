68b0c2717398e0e7cd7415ee75f70341f1e884b5
Bala <balasubramanian2003130@gmail.com>
2026-04-17 10:37:11 +0530

    Refactor arithmetic methods and add division

--- Test.java
+++ Test.java
@@ -1,35 +1,38 @@
 public class Test {
 
-  static void add(){
-
-    int a=10;
-    int b=20;
-    int result = a+b;
-    System.out.println("The sum is " +result);
-    
-  }
-  static void sub(){
-
-    int c=20;
-    int d=10;
-    int result1 = c-d;
-    System.out.println("The diff is " +result1);
-    
+  static void add() {
+    int a = 10;
+    int b = 20;
+    int result = a + b;
+    System.out.println("The sum is " + result);
   }
 
-   static void multi(){
-
-    int x=5;
-    int y=8;
-    int result2 = x*y;
-    System.out.println("The multiplcation result is: " +result2);
-    
+  static void sub() {
+    int c = 20;
+    int d = 10;
+    int result1 = c - d;
+    System.out.println("The diff is " + result1);
   }
 
-  public static void main(String[] args)
-  {
+  static void multi() {
+    int x = 5;
+    int y = 8;
+    int result2 = x * y;
+    System.out.println("The multiplication result is: " + result2);
+  }
+
+  
+  static void div() {
+    double p = 20;
+    double q = 5;
+    double result3 = p / q;
+    System.out.println("The division result is: " + result3);
+  }
+
+  public static void main(String[] args) {
     add();
     sub();
     multi();
+    div(); 
   }
 }
